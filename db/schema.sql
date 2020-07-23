6971b372c84ce9b652aa1606ec29d0abaecc4410
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-07-23 11:23:12 -0500

    added basic db structure and connection.js details

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- drop the database if it exists --
+DROP DATABASE IF EXISTS nintracker_db;
+-- create the database --
+CREATE DATABASE nintracker_db;
+
+
+-- 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+FLUSH PRIVILEGES;
+
+-- use database nintracker --
+USE nintracker_db;
+
+-- create table for users --
+CREATE TABLE users (
+    id INTEGER (11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	email VARCHAR (255) NOT NULL,
+	password VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE games (
+    id INTEGER (11) NOT NULL PRIMARY KEY,
+    title VARCHAR (255) NOT NULL,
+    platforms VARCHAR (255) NOT NULL,
+    genres VARCHAR (255) NOT NULL,
+    background_image VARCHAR (255), 
+    userID INTEGER (11) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users (id) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, password) VALUES ("place@holder.com", "passtheword");
+INSERT INTO users (email, password) VALUES ("holder@place.com", "passtheword2me");
+INSERT INTO users (email, password) VALUES ("awesome@email.com", "secretpassword");
+INSERT INTO users (email, password) VALUES ("another@email.com", "anotherpassword");
+
