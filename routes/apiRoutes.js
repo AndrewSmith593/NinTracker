912c3f98a94d69c2165e8a75720ead96f7ecf10c
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-07-22 20:21:01 -0500

    created basic MVC file structure

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+// Require in express router
+const router = require("express").Router();
+
+
+// CRUD Operations
+
+// CREATE
+// POST new games that the user saves into their database 
+
+
+
+// READ
+// GET saved and completed games for the user
+
+// UPDATE
+// PUT new updates to a game table
+
+// DELETE
+// DELETE method to remove a game from a table/list
