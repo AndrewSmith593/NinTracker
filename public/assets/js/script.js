bbe9ce5743e84a67e51f27361c790f7d6e18bf8b
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-07-26 18:06:26 -0500

    added more data to game cards and updated the search bar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   var releaseDate = $("#releaseDate")
   // metacritic score
   var score = $("#score")
-  
+
   // searchedCharacter = searchedCharacter.replace(/\s+/g, "+").toLowerCase();
   // event listenter for button press
   $("#searchButton").click(function () {
@@ -26,25 +26,55 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      JSON.stringify(response)
-      console.log(response)
-      gameSearch.replace("")
-      gameTitle.text(response.results[0].name);
-      recentCon.text("Most recent console: " + response.results[0].platforms[0].platform.name)
-      gameImage.attr("src", response.results[0].background_image);
-      
+      JSON.stringify(response);
+      console.log(response);
+      $('#gameSearch').val('');
+      var firstResult = response.results[0]
+      var conIndex = firstResult.platforms.length - 1
+      gameTitle.text(firstResult.name);
+      originalCon.text(`Original console: ${firstResult.platforms[conIndex].platform.name}`)
+      recentCon.text(`Most recent console: ${firstResult.platforms[0].platform.name}`);
+      releaseDate.text("Released on: " + firstResult.released)
+      gameImage.attr("src", firstResult.background_image);
+      // genres.text(`Consoles: ${genreList}`)
+
+      function returnGenres() {
+        var genreList = [];
+        var genreArray = firstResult.genres
+        for (let i = 0; i < genreArray.length; i++) {
+
+          console.log(genreArray)
+          var currentGenre = (` ${genreArray[i].name}`);
+          
+          genreList.push(currentGenre)
+          
+        }
+        console.log(genreList)
+        genres.text(`Genres: ${genreList}`)
+        return genreList;
+
+      }
+      returnGenres()
+
+      if (firstResult.metacritic == null) {
+        score.text(`Average Rating: ${firstResult.rating}/5`)
+      } else {
+        score.text(`Metacritic Score: ${firstResult.metacritic}/100`)
+      }
+
+
       // This filter takes in the original RAWG response and returns an array of games that are only on Nintendo systems as the var filteredGames
       var filteredGames = response.results.filter(function (results) {
         console.log("results:")
-        console.log(results)
+        // console.log(results)
         var platformArray = results.platforms.filter(function (data) {
           return data.platform.id == 7 || data.platform.id == 8 || data.platform.id == 10 || data.platform.id == 11 || data.platform.id == 24 || data.platform.id == 43 || data.platform.id == 79 || data.platform.id == 49 || data.platform.id == 26 || data.platform.id == 105 || data.platform.id == 83;
 
         })
-        console.log(`here is the platform:`)
-        console.log(platformArray)
+        // console.log(`here is the platform:`)
+        // console.log(platformArray)
       });
-      console.log(filteredGames)
+      // console.log(filteredGames)
     });
     // info to append to page
 
