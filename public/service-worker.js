b5b7913ec2065dabcf5d80a712d00c50c608b30a
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-09-19 09:51:07 -0500

    screenshot for readme

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DARA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+    "/",
+    "../models/index.js",
+    "/manifest.json"
+
+];
+
+self.addEventListener("install", function(event) {
+    // perform install steps
+})
